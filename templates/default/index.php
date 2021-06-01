7ac013d5218b24cfe7abd7f02a9bcd8f29dbe121
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-06-01 20:12:13 +0700

    update timer

--- templates/default/index.php
+++ templates/default/index.php
@@ -3,7 +3,7 @@
 </div>
 
 <script>
-var countdown
+var countdown, scoreboardInterval
 async function getScoreboard()
 {
     var request = await fetch('index.php?realtime-request=true')
@@ -11,6 +11,7 @@ async function getScoreboard()
 
     if(response)
     {
+        clearInterval(scoreboardInterval)
         response = JSON.parse(response)
         var timer = 5
         document.querySelector('.scoreboards').innerHTML = '<h2>'+timer+'</h2>'
@@ -22,33 +23,15 @@ async function getScoreboard()
                 document.querySelector('.scoreboards').innerHTML = '<h2>'+response.name+' ('+response.total_score+')</h2>'
                 setTimeout(e => {
                     document.querySelector('.scoreboards').innerHTML = '<h2>Menunggu...</h2>'
+                    scoreboardInterval = setInterval(getScoreboard,1000)
                 }, 10000)
             }
             else
                 document.querySelector('.scoreboards').innerHTML = '<h2>'+timer+'</h2>'
         },1000)
-        // var table = '<table class="table table-bordered">'
-        // table += '<thead>'
-        // table += '<tr>'
-        // table += '<td>Nama</td>'
-        // table += '<td>Nilai</td>'
-        // table += '</tr>'
-        // table += '</thead>'
-        // table += '<tbody>'
-        // table += '<tr>'
-        // table += '<td>'+response.name+'</td>'
-        // table += '<td>'+response.total_score+'</td>'
-        // table += '</tr>'
-
-        // table += '</tbody></table>'
-
-        // document.querySelector('.scoreboards').innerHTML = '<div class="p-10 bg-white">'+table+'</div>'
     }
-
-
-    // console.log(response)
 }
 
 getScoreboard()
-setInterval(getScoreboard,1000)
+scoreboardInterval = setInterval(getScoreboard,1000)
 </script>
